42066f166d09bfaa3e292e7845f2922a87c0537c
timriffe <tim.riffe@gmail.com>
2023-02-02 13:41:16 +0100

    use @importFrom HMDHFDplus in roxygen instead of ::

--- R/GetData.R
+++ R/GetData.R
@@ -10,7 +10,7 @@
 #' @param pw_HFD Password to access HFD, provided at registration
 #' @return \item{data_raw}{5 objects: country,life tables females,life tables males,fertility rates,female population (from HFD): exposures}
 #' @author Frans Willekens
-#' 
+#' @importFrom HMDHFDplus getHMDcountries readHMDweb readHFDweb
 #' @examples
 #' 
 #' 
@@ -20,10 +20,9 @@
 GetData <- function(country,user,pw_HMD,pw_HFD)
 {
 # ==============  Part A Extract data from HMD and HFD  ==================
-requireNamespace("HMDHFDplus")
 
 # help function to list the available countries
-countries <- HMDHFDplus::getHMDcountries()$CNTRY
+countries <- getHMDcountries()$CNTRY
 
 # ====================  Get data from HMD and HFD: data_raw  =================
 #country <- "USA"
@@ -32,14 +31,14 @@ dataLH <- NULL
 rates <- NULL
 # =============  Read life tables and get death rates  ============
 message (paste ("Extract data from HMD and HFD for ",country,sep=""))
-df <- HMDHFDplus::readHMDweb(CNTRY=country,item="fltper_1x1",username=user,password=pw_HMD,fixup=TRUE)
-dm <- HMDHFDplus::readHMDweb(CNTRY=country,item="mltper_1x1",username=user,password=pw_HMD,fixup=TRUE)
+df <- readHMDweb(CNTRY=country,item="fltper_1x1",username=user,password=pw_HMD,fixup=TRUE)
+dm <- readHMDweb(CNTRY=country,item="mltper_1x1",username=user,password=pw_HMD,fixup=TRUE)
 
 # ============  Read conditional age-specific fertility rates ===============
-fert_rates <- HMDHFDplus::readHFDweb(CNTRY=country,item="mi",username=user,password=pw_HFD,fixup=TRUE)
+fert_rates <- readHFDweb(CNTRY=country,item="mi",username=user,password=pw_HFD,fixup=TRUE)
 
 # ======  Extract population from HMD  ====
-dpopHMD <- HMDHFDplus::readHMDweb(CNTRY=country,item="Population",username=user,password=pw_HMD,fixup=TRUE) 
+dpopHMD <- readHMDweb(CNTRY=country,item="Population",username=user,password=pw_HMD,fixup=TRUE) 
 
 # ============  Create raw data file  =================
 data_raw <- list (country=country,LTf=df,LTm=dm,fert_rates=fert_rates,dpopHMD=dpopHMD)
@@ -49,19 +48,19 @@ itest <- 1
 if (itest==50)
 {
 # Read life table for males and females combined
-db <- HMDHFDplus::readHMDweb(CNTRY=country,item="bltper_1x1",username=user,password=pw_HMD,fixup=TRUE) 
+db <- readHMDweb(CNTRY=country,item="bltper_1x1",username=user,password=pw_HMD,fixup=TRUE) 
 # Read fertility table
-fertTable <- HMDHFDplus::readHFDweb(CNTRY=country,item="pft",username=user,password=pw_HFD,fixup=TRUE)
+fertTable <- readHFDweb(CNTRY=country,item="pft",username=user,password=pw_HFD,fixup=TRUE)
 attr(fertTable, "data") <- paste ("HFD",country,sep=" ")
 
 # Read age-specific fertility rates
-asfrVV <- HMDHFDplus::readHFDweb(CNTRY=country,item="asfrVV",username=user,password=pw_HFD,fixup=TRUE)
+asfrVV <- readHFDweb(CNTRY=country,item="asfrVV",username=user,password=pw_HFD,fixup=TRUE)
 # Read TFR (period)
-tfrRR <- HMDHFDplus::readHFDweb(CNTRY=country,item="tfrRR",username=user,password=pw_HFD,fixup=TRUE)
+tfrRR <- readHFDweb(CNTRY=country,item="tfrRR",username=user,password=pw_HFD,fixup=TRUE)
 
 # ======  Part B Extract female population exposure by parity from HFD ====
 #  Population from HFD: Exposure in conditional fertility rates 
-dpopHFD <- HMDHFDplus::readHFDweb(CNTRY=country,item="exposRRpa",username=user,password=pw_HFD,fixup=TRUE)
+dpopHFD <- readHFDweb(CNTRY=country,item="exposRRpa",username=user,password=pw_HFD,fixup=TRUE)
 
 data_raw <- list (country=country,refyear=refyear_data,LTf=df,LTm=dm,LTcom=db,
 fertTable=fertTable,fert_rates=fert_rates,asfrVV=asfrVV,tfrRR=tfrRR,dpopHMD=dpopHMD,dpopHFD=dpopHFD)
